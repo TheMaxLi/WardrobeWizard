99884cd0d68125ffe03f45769104928387a7a53e
Max <maxli88488@gmail.com>
2024-10-05 20:01:32 -0700

    chagne file name for merge

--- ww-web-app/src/routes/+page.svelte
+++ ww-web-app/src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import PoseEstimation from '$lib/components/PoseEstimation.svelte';
+	import PoseEstimationTensor from '$lib/components/PoseEstimationTensor.svelte';
 	import ClothingSegmentation from '$lib/components/ClothingSegmentation.svelte';
 	import TryOn from '$lib/components/TryOn.svelte';
 </script>
 
 <div class="flex flex-col">
-	<PoseEstimation />
+	<PoseEstimationTensor />
 	<ClothingSegmentation />
 	<TryOn />
 </div>
