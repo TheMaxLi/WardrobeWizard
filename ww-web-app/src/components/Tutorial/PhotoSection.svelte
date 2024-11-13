f02d001c19e58f0f843cd9f09f20af86e3d5a869
Max <maxli88488@gmail.com>
2024-11-12 21:06:15 -0800

    add images to tutorial

--- ww-web-app/src/components/Tutorial/PhotoSection.svelte
+++ ww-web-app/src/components/Tutorial/PhotoSection.svelte
@@ -1,20 +1,29 @@
 <script>
+	import shirtClick from './../../../public/static/shirt-click.jpg';
+	import shirtShorts from './../../../public/static/shirt-shorts.jpg';
+	import heart from './../../../public/static/heart.jpg';
+	import camera from './../../../public/static/camera.jpg';
+
 	const steps = [
 		{
 			id: 1,
-			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm'
+			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm',
+			img: shirtClick
 		},
 		{
 			id: 2,
-			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm'
+			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm',
+			img: shirtShorts
 		},
 		{
 			id: 3,
-			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm'
+			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm',
+			img: heart
 		},
 		{
 			id: 4,
-			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm'
+			text: 'Meow meow meowmeowmeowm meowmeowmmoem wm',
+			img: camera
 		}
 	];
 </script>
@@ -24,7 +33,8 @@
 	<div class="grid grid-cols-2 lg:grid-cols-4 gap-6">
 		{#each steps as step (step.id)}
 			<div class="rounded-lg p-2">
-				<div class="aspect-square w-full bg-gray-200 mb-4 rounded-lg"></div>
+				<img src={step.img} alt="" class="aspect-square w-full bg-gray-200 mb-4 rounded-l" />
+				<!-- <div class="aspect-square w-full bg-gray-200 mb-4 rounded-lg"></div> -->
 				<div class="space-x-2 flex items-start justify-start">
 					<div class="text-lg font-medium">{step.id}.</div>
 					<p class="text-gray-600 text-xs break-words">{step.text}</p>
